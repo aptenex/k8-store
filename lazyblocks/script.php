c9de7bc7aacf7ade5d79115c133f3074934e945a
Sam <samvaughton@users.noreply.github.com>
2021-07-12 15:32:08 +0100

    Converted the script to use $args provided by WP CLI

--- lazyblocks/script.php
+++ lazyblocks/script.php
@@ -1,17 +1,17 @@
 <?php
 
-// remove all existing lazy blocks
+$file = $args[0];
 
 // make sure it works
-json_decode(file_get_contents('/opt/bitnami/lazyblocks.json'), true, 512, JSON_THROW_ON_ERROR);
+json_decode(file_get_contents($file), true, 512, JSON_THROW_ON_ERROR);
 
 echo "running lazyblocks delete + import \n";
 $_POST[ 'lzb_tools_import_nonce' ] = wp_create_nonce('lzb-tools-import-nonce');
 $_FILES['lzb_tools_import_json'] = [
-    'size' => strlen(file_get_contents('/opt/bitnami/lazyblocks.json')),
+    'size' => strlen(file_get_contents($file)),
     'error' => false,
-    'name' => 'lazyblocks.json',
-    'tmp_name' => '/opt/bitnami/lazyblocks.json',
+    'name' => 'data.json',
+    'tmp_name' => $file,
 ];
 
 $tools = new LazyBlocks_Tools();
