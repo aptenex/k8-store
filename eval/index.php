f68165304512f1b7573d9a3935a5588b25d8a3bf
Sam Vaughton <samvaughton@gmail.com>
2021-07-14 14:49:22 +0100

    different method to load acf field group functions

--- eval/index.php
+++ eval/index.php
@@ -42,8 +42,8 @@ switch ($evalCommand) {
     case 'acf-field-group-sync':
         $_GET['acfsync'] = [];
 
-
-        acf();
+        include_once( ACF_PATH . 'includes/acf-utility-functions.php');
+        acf_include('includes/admin/admin-field-groups.php');
 
         $tools = new \ACF_Admin_Field_Groups();
 
